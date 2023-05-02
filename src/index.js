63aaf3bf48d80ff7b046a6a4bed7fc06ac192bb4
Bhuvnesh Sharma <bhuvi844964@gmail.com>
2023-05-02 14:13:37 +0530

    update new

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ app.use(express.json())
 mongoose.connect("mongodb+srv://avi-sin:CJTIF4CupXQdRKHV@cluster0.ovf3r.mongodb.net/group16Database", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected ✅"))
+.then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use('/', route)
+app.use('/', route)                       
 
 
 app.listen(process.env.PORT || 3000, function () {
